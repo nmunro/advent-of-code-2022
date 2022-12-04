01c8a0299ef5487ab4b145ae73e5ebad307b925d
NMunro <neilmunro@gmail.com>
2022-12-04 11:12:54 +0000

    day 3 part 2

--- src/day-3.lisp
+++ src/day-3.lisp
@@ -13,8 +13,26 @@
   (let ((midpoint (/ (length item) 2)))
     `(,(subseq item 0 midpoint) ,(subseq item midpoint))))
 
-(defun find-intersection (item1 item2)
-  (car (intersection (coerce item1 'list) (coerce item2 'list))))
+(defun find-intersection (item1 item2 &key (remove-duplicates t))
+  (if remove-duplicates
+      (car (intersection (coerce item1 'list) (coerce item2 'list)))
+      (intersection (coerce item1 'list) (coerce item2 'list))))
 
-(let ((items (get-data #p"~/quicklisp/local-projects/advent-of-code-2022/data/data-day-3.txt")))
-  (loop :for item :in items :summing (1+ (position (apply #'find-intersection (split-item item)) +priority+ :test #'equal))))
+(defun find-priority (item)
+  (1+ (position item +priority+ :test #'equal)))
+
+(defun find-badge (item1 item2 item3)
+  (let ((intersection-1 (find-intersection (coerce item1 'list) (coerce item2 'list) :remove-duplicates nil))
+      (intersection-2 (find-intersection (coerce item2 'list) (coerce item3 'list) :remove-duplicates nil)))
+    (car (intersection intersection-1 intersection-2))))
+
+(defun part-1 (path)
+  (let ((items (get-data path)))
+    (loop :for item :in items :summing (find-priority (apply #'find-intersection (split-item item))))))
+
+(defun part-2 (path)
+  (let* ((batches (serapeum:batches (get-data path) 3)))
+    (loop :for batch :in batches :summing (find-priority (apply #'find-badge batch)))))
+
+(part-1 #p"~/quicklisp/local-projects/advent-of-code-2022/data/data-day-3.txt")
+(part-2 #p"~/quicklisp/local-projects/advent-of-code-2022/data/data-day-3.txt")
